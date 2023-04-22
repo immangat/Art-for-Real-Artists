5c3d78e8c4160ff1fa8c10b794c87b7e43d963d4
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-04-21 23:50:19 -0700

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const Footer = () => {
+  return (
+    <footer>
+      <p>This is my footer</p>
+    </footer>
+  );
 }
+
+export default function App({ Component, pageProps }) {
+  return (
+  <div>
+    <Component {...pageProps} />
+    <footer>
+      <p>This is my footer</p>
+    </footer>
+    </div>);
+}
+
+
